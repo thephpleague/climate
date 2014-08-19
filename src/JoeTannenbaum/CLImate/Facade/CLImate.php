2f830d3e936cae5023868ce4b16fb2e9f1c0c824
Joe Tannenbaum <josephtannenbaum@gmail.com>
2014-08-18 22:22:39 -0400

    added some of that laravel sugar

---
+++ src/JoeTannenbaum/CLImate/Facade/CLImate.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace JoeTannenbaum\CLImate\Facade;
+
+use Illuminate\Support\Facades\Facade;
+
+class CLImate extends Facade {
+
+    protected static function getFacadeAccessor() { return 'CLImate'; }
+
+}
