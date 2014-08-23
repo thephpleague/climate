06d64c8957a32d36f76c8cb93221956ff0d24edf
Joe Tannenbaum <josephtannenbaum@gmail.com>
2014-08-23 10:07:00 -0400

    simplified dump test since it was not formatting consistently

--- tests/DumpTest.php
+++ tests/DumpTest.php
@@ -11,35 +11,19 @@ class DumpTest extends PHPUnit_Framework_TestCase
 
     /** @test */
 
-    public function it_can_output_an_object_as_json()
+    public function it_can_dump_a_variable()
     {
         ob_start();
 
-        $this->cli->dump([
-                    'Thing 1',
-                    'Thing 2',
-                    'Thing 3',
-                    'Thing 4',
-                ]);
+        $this->cli->dump('This thing');
 
         $result = ob_get_contents();
 
         ob_end_clean();
 
-        file_put_contents('wat', $result);
-
         $should_be = [];
 
-        $should_be[] = 'array(4) {';
-        $should_be[] = '  [0]=>';
-        $should_be[] = '  string(7) "Thing 1"';
-        $should_be[] = '  [1]=>';
-        $should_be[] = '  string(7) "Thing 2"';
-        $should_be[] = '  [2]=>';
-        $should_be[] = '  string(7) "Thing 3"';
-        $should_be[] = '  [3]=>';
-        $should_be[] = '  string(7) "Thing 4"';
-        $should_be[] = '}';
+        $should_be[] = 'string(10) "This thing"';
         $should_be[] = '';
 
         $should_be = "\e[m" . implode( "\n", $should_be ) . "\e[0m\n";
