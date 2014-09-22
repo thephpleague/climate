39224402938fc9128eef5bce8602bfe5972149b4
Joe Tannenbaum <joe@joe.codes>
2014-09-21 20:46:23 -0400

    router interface doc blocks

--- src/TerminalObject/Router/RouterInterface.php
+++ src/TerminalObject/Router/RouterInterface.php
@@ -4,9 +4,21 @@ namespace League\CLImate\TerminalObject\Router;
 
 interface RouterInterface {
 
+    /**
+     * @return string
+     */
+
     public function path($class);
 
+    /**
+     * @return boolean
+     */
+
     public function exists($class);
+
+    /**
+     * @return null|\League\CLImate\TerminalObject\Dynamic
+     */
 
     public function execute($obj);
 
