be5d5de671b19cbe7cf74824c9c9fd395a91486a
Joe Tannenbaum <joe@joe.codes>
2014-09-22 06:48:49 -0400

    fixed up column widths

--- src/TerminalObject/Columns.php
+++ src/TerminalObject/Columns.php
@@ -36,15 +36,17 @@ class Columns extends BaseTerminalObject
 
     public function result()
     {
-        $column_width = $this->getColumnWidth();
+        $column_width = $this->getColumnWidth($this->data);
         $max_rows     = $this->getMaxRows($column_width);
 
         $this->data   = array_chunk($this->data, $max_rows);
 
+        $column_widths = $this->getColumnWidths();
+
         $output = [];
 
         for ($i = 0; $i < $max_rows; $i++) {
-            $output[] = $this->getRow($i, $column_width);
+            $output[] = $this->getRow($i, $column_widths);
         }
 
         return $output;
@@ -55,15 +57,16 @@ class Columns extends BaseTerminalObject
      *
      * @param integer $key
      * @param integer $column_width
+     * @return string
      */
 
-    protected function getRow($key, $column_width)
+    protected function getRow($key, $column_widths)
     {
         $row = [];
 
         for ($j = 0; $j < $this->count; $j++) {
             if (array_key_exists($key, $this->data[$j])) {
-                $row[] = $this->pad($this->data[$j][$key], $column_width);
+                $row[] = $this->pad($this->data[$j][$key], $column_widths[$j]);
             }
         }
 
@@ -73,22 +76,39 @@ class Columns extends BaseTerminalObject
     /**
      * Get the standard column width
      *
+     * @param array $data
      * @return integer
      */
 
-    protected function getColumnWidth()
+    protected function getColumnWidth($data)
     {
-        $column_width = array_map([$this, 'lengthWithoutTags'], $this->data);
+        $column_width = array_map([$this, 'lengthWithoutTags'], $data);
 
         // Return the maximum width plus a buffer
         return max($column_width) + 5;
     }
 
     /**
+     * Get an array of each column's width
+     *
+     * @return array
+     */
+
+    protected function getColumnWidths()
+    {
+        $column_widths = [];
+
+        for ($i = 0; $i < $this->count; $i++) {
+            $column_widths[] = $this->getColumnWidth($this->data[$i]);
+        }
+
+        return $column_widths;
+    }
+
+    /**
      * Get the number of rows per column
      *
      * @param integer $column_width
-     *
      * @return integer
      */
 
--- tests/ColumnTest.php
+++ tests/ColumnTest.php
@@ -20,16 +20,16 @@ class ColumnTest extends TestBase
                         'too',
                         'and',
                         'also',
-                        'this much longer thing',
+                        'this is much longer',
                     ]);
 
         $result = ob_get_contents();
 
         ob_end_clean();
 
-        $should_be = "\e[mthis                  thing                 and                   \e[0m\n";
-        $should_be .= "\e[mthat                  this                  also                  \e[0m\n";
-        $should_be .= "\e[mother                 too                   this much longer thing\e[0m\n";
+        $should_be = "\e[mthis      thing     and                     \e[0m\n";
+        $should_be .= "\e[mthat      this      also                    \e[0m\n";
+        $should_be .= "\e[mother     too       this is much longer     \e[0m\n";
 
         $this->assertSame($should_be, $result);
     }
@@ -49,7 +49,7 @@ class ColumnTest extends TestBase
                         'too',
                         'and',
                         'also',
-                        'this much longer thing',
+                        'this is much longer',
                     ], 2);
 
         $this->cli->columns([
@@ -61,27 +61,27 @@ class ColumnTest extends TestBase
                         'too',
                         'and',
                         'also',
-                        'this much longer thing',
+                        'this is much longer',
                     ], 1);
 
         $result = ob_get_contents();
 
         ob_end_clean();
 
-        $should_be = "\e[mthis                  too                   \e[0m\n";
-        $should_be .= "\e[mthat                  and                   \e[0m\n";
-        $should_be .= "\e[mother                 also                  \e[0m\n";
-        $should_be .= "\e[mthing                 this much longer thing\e[0m\n";
-        $should_be .= "\e[mthis                  \e[0m\n";
-        $should_be .= "\e[mthis                  \e[0m\n";
-        $should_be .= "\e[mthat                  \e[0m\n";
-        $should_be .= "\e[mother                 \e[0m\n";
-        $should_be .= "\e[mthing                 \e[0m\n";
-        $should_be .= "\e[mthis                  \e[0m\n";
-        $should_be .= "\e[mtoo                   \e[0m\n";
-        $should_be .= "\e[mand                   \e[0m\n";
-        $should_be .= "\e[malso                  \e[0m\n";
-        $should_be .= "\e[mthis much longer thing\e[0m\n";
+        $should_be = "\e[mthis      too                     \e[0m\n";
+        $should_be .= "\e[mthat      and                     \e[0m\n";
+        $should_be .= "\e[mother     also                    \e[0m\n";
+        $should_be .= "\e[mthing     this is much longer     \e[0m\n";
+        $should_be .= "\e[mthis      \e[0m\n";
+        $should_be .= "\e[mthis                    \e[0m\n";
+        $should_be .= "\e[mthat                    \e[0m\n";
+        $should_be .= "\e[mother                   \e[0m\n";
+        $should_be .= "\e[mthing                   \e[0m\n";
+        $should_be .= "\e[mthis                    \e[0m\n";
+        $should_be .= "\e[mtoo                     \e[0m\n";
+        $should_be .= "\e[mand                     \e[0m\n";
+        $should_be .= "\e[malso                    \e[0m\n";
+        $should_be .= "\e[mthis is much longer     \e[0m\n";
 
         $this->assertSame($should_be, $result);
     }
@@ -101,16 +101,16 @@ class ColumnTest extends TestBase
                         'too',
                         'and',
                         'also',
-                        'this much longer thinø',
+                        'this is much longeø',
                     ]);
 
         $result = ob_get_contents();
 
         ob_end_clean();
 
-        $should_be = "\e[mthis                  thing                 and                   \e[0m\n";
-        $should_be .= "\e[mthat                  this                  also                  \e[0m\n";
-        $should_be .= "\e[mother                 too                   this much longer thinø\e[0m\n";
+        $should_be = "\e[mthis      thing     and                     \e[0m\n";
+        $should_be .= "\e[mthat      this      also                    \e[0m\n";
+        $should_be .= "\e[mother     too       this is much longeø     \e[0m\n";
 
         $this->assertSame($should_be, $result);
     }
