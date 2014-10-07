d1f8aed824b1ad715206a624bd8012b92f110545
Joe Tannenbaum <joe@joe.codes>
2014-10-06 20:35:46 -0400

    fixed column tests

--- tests/ColumnTest.php
+++ tests/ColumnTest.php
@@ -9,7 +9,9 @@ class ColumnTest extends TestBase
 
     public function it_can_output_columns()
     {
-        ob_start();
+        $this->shouldWrite("\e[mthis      thing     and                     \e[0m");
+        $this->shouldWrite("\e[mthat      this      also                    \e[0m");
+        $this->shouldWrite("\e[mother     too       this is much longer     \e[0m");
 
         $this->cli->columns([
                         'this',
@@ -22,23 +24,26 @@ class ColumnTest extends TestBase
                         'also',
                         'this is much longer',
                     ]);
-
-        $result = ob_get_contents();
-
-        ob_end_clean();
-
-        $should_be = "\e[mthis      thing     and                     \e[0m\n";
-        $should_be .= "\e[mthat      this      also                    \e[0m\n";
-        $should_be .= "\e[mother     too       this is much longer     \e[0m\n";
-
-        $this->assertSame($should_be, $result);
     }
 
     /** @test */
 
     public function it_can_output_a_specific_number_of_columns()
     {
-        ob_start();
+        $this->shouldWrite("\e[mthis      too                     \e[0m");
+        $this->shouldWrite("\e[mthat      and                     \e[0m");
+        $this->shouldWrite("\e[mother     also                    \e[0m");
+        $this->shouldWrite("\e[mthing     this is much longer     \e[0m");
+        $this->shouldWrite("\e[mthis      \e[0m");
+        $this->shouldWrite("\e[mthis                    \e[0m");
+        $this->shouldWrite("\e[mthat                    \e[0m");
+        $this->shouldWrite("\e[mother                   \e[0m");
+        $this->shouldWrite("\e[mthing                   \e[0m");
+        $this->shouldWrite("\e[mthis                    \e[0m");
+        $this->shouldWrite("\e[mtoo                     \e[0m");
+        $this->shouldWrite("\e[mand                     \e[0m");
+        $this->shouldWrite("\e[malso                    \e[0m");
+        $this->shouldWrite("\e[mthis is much longer     \e[0m");
 
         $this->cli->columns([
                         'this',
@@ -63,34 +68,15 @@ class ColumnTest extends TestBase
                         'also',
                         'this is much longer',
                     ], 1);
-
-        $result = ob_get_contents();
-
-        ob_end_clean();
-
-        $should_be = "\e[mthis      too                     \e[0m\n";
-        $should_be .= "\e[mthat      and                     \e[0m\n";
-        $should_be .= "\e[mother     also                    \e[0m\n";
-        $should_be .= "\e[mthing     this is much longer     \e[0m\n";
-        $should_be .= "\e[mthis      \e[0m\n";
-        $should_be .= "\e[mthis                    \e[0m\n";
-        $should_be .= "\e[mthat                    \e[0m\n";
-        $should_be .= "\e[mother                   \e[0m\n";
-        $should_be .= "\e[mthing                   \e[0m\n";
-        $should_be .= "\e[mthis                    \e[0m\n";
-        $should_be .= "\e[mtoo                     \e[0m\n";
-        $should_be .= "\e[mand                     \e[0m\n";
-        $should_be .= "\e[malso                    \e[0m\n";
-        $should_be .= "\e[mthis is much longer     \e[0m\n";
-
-        $this->assertSame($should_be, $result);
     }
 
     /** @test */
 
     public function it_can_handle_multibyte_strings()
     {
-        ob_start();
+        $this->shouldWrite("\e[mthis      thing     and                     \e[0m");
+        $this->shouldWrite("\e[mthat      this      also                    \e[0m");
+        $this->shouldWrite("\e[mother     too       this is much longeø     \e[0m");
 
         $this->cli->columns([
                         'this',
@@ -103,16 +89,6 @@ class ColumnTest extends TestBase
                         'also',
                         'this is much longeø',
                     ]);
-
-        $result = ob_get_contents();
-
-        ob_end_clean();
-
-        $should_be = "\e[mthis      thing     and                     \e[0m\n";
-        $should_be .= "\e[mthat      this      also                    \e[0m\n";
-        $should_be .= "\e[mother     too       this is much longeø     \e[0m\n";
-
-        $this->assertSame($should_be, $result);
     }
 
 }
