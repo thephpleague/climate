642f1c6a99d28c1120186a617ea9766f7fa99f82
Craig Duncan <git@duncanc.co.uk>
2014-11-25 23:00:10 +0000

    Created a tab() method

---
+++ src/TerminalObject/Tab.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace League\CLImate\TerminalObject;
+
+/**
+ * Tab class to enable tabs to be output without using the escape character.
+ */
+class Tab extends Br
+{
+
+    /**
+     * Check if this object requires a new line should be added after the output.
+     *
+     * @return boolean
+     */
+
+    public function sameLine()
+    {
+        return true;
+    }
+
+    /**
+     * Return the relevant number of tab characters.
+     *
+     * @return string
+     */
+
+    public function result()
+    {
+        return str_repeat("\t", $this->count);
+    }
+}
