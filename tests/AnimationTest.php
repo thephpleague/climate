6109198f397c398b12c651ac0eb12c1241dd68b0
Joe Tannenbaum <joe@joe.codes>
2015-01-12 07:13:06 -0500

    added enter tests

--- tests/AnimationTest.php
+++ tests/AnimationTest.php
@@ -9,6 +9,11 @@ class AnimationTest extends TestBase
 
     use ExitToTopFrames, ExitToBottomFrames;
 
+    protected function emptyFrame()
+    {
+        $this->shouldWrite("\e[m\e[0m", 6);
+    }
+
     /** @test */
 
     public function it_can_exit_to_top()
@@ -45,6 +50,36 @@ class AnimationTest extends TestBase
         $this->exitBottomFrame6();
 
         $this->cli->animation('404')->exitToBottom();
+    }
+
+    /** @test */
+
+    public function it_can_enter_from_bottom()
+    {
+        $this->emptyFrame();
+        $this->exitBottomFrame5();
+        $this->exitBottomFrame4();
+        $this->exitBottomFrame3();
+        $this->exitBottomFrame2();
+        $this->exitBottomFrame1();
+        $this->fullArtExitBottomPlus();
+
+        $this->cli->animation('404')->enterFromBottom();
+    }
+
+    /** @test */
+
+    public function it_can_enter_from_top()
+    {
+        $this->emptyFrame();
+        $this->exitTopFrame5();
+        $this->exitTopFrame4();
+        $this->exitTopFrame3();
+        $this->exitTopFrame2();
+        $this->exitTopFrame1();
+        $this->fullArtExitTopPlus();
+
+        $this->cli->animation('404')->enterFromTop();
     }
 
 }
