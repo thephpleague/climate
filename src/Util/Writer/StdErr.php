969dc16f7407b92d3897a0ce125483313551e59e
Terah <terry@terah.com.au>
2015-02-17 12:14:23 +1000

    Adding stdErr file

---
+++ src/Util/Writer/StdErr.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace League\CLImate\Util\Writer;
+
+class StdErr implements WriterInterface
+{
+    /**
+     * Write the content to the stream
+     *
+     * @param  string $content
+     */
+
+    public function write($content)
+    {
+        fwrite(\STDERR, $content);
+    }
+
+}
