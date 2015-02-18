5245a41119facb132ebc5df1939e45beb08b7bf7
Craig Duncan <git@duncanc.co.uk>
2015-02-18 13:08:36 +0000

    Minor CS fixes
    Space before closing braces, and global constants using a leading backslash

--- src/Util/Writer/StdOut.php
+++ src/Util/Writer/StdOut.php
@@ -8,10 +8,11 @@ class StdOut implements WriterInterface
      * Write the content to the stream
      *
      * @param  string $content
+     *
+     * @return void
      */
     public function write($content)
     {
-        fwrite(STDOUT, $content);
+        fwrite(\STDOUT, $content);
     }
-
 }
