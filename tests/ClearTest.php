7d35ca2a7246de07caaab145837b6e5fb5bc0bb0
Joe Tannenbaum <joe@joe.codes>
2015-02-17 21:38:53 -0500

    put cursor at top of the screen after clear, keep on same line. closes #53.

--- tests/ClearTest.php
+++ tests/ClearTest.php
@@ -8,7 +8,8 @@ class ClearTest extends TestBase
     /** @test */
     public function it_can_clear_the_terminal()
     {
-        $this->shouldWrite("\e[m\e[2J\e[0m");
+        $this->output->shouldReceive("sameLine")->andReturn(true);
+        $this->shouldWrite("\e[m\e[H\e[2J\e[0m");
         $this->cli->clear();
     }
 
