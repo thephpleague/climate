8dc147bf81d3fc326fe4e76cf7ad4b7cb66b2d45
Joe Tannenbaum <joe@joe.codes>
2015-03-01 09:43:23 -0500

    broke up animation class, extracted to Keyframe class

--- src/TerminalObject/Dynamic/Animation.php
+++ src/TerminalObject/Dynamic/Animation.php
@@ -2,25 +2,28 @@
 
 namespace League\CLImate\TerminalObject\Dynamic;
 
+use League\CLImate\TerminalObject\Dynamic\Animation\Keyframe;
 use League\CLImate\TerminalObject\Helper\Art;
-use League\CLImate\TerminalObject\Helper\StringLength;
 use League\CLImate\TerminalObject\Helper\Sleeper;
 
 class Animation extends DynamicTerminalObject
 {
-    use Art, StringLength;
+    use Art;
 
     /**
      * @var \League\CLImate\TerminalObject\Helper\Sleeper $sleeper
      */
     protected $sleeper;
 
-    public function __construct($art, Sleeper $sleeper = null)
+    protected $keyframes;
+
+    public function __construct($art, Sleeper $sleeper = null, Keyframe $keyframes = null)
     {
         // Add the default art directory
         $this->addDir(__DIR__ . '/../../ASCII');
 
         $this->setSleeper($sleeper);
+        $this->setKeyFrames($keyframes);
 
         $this->art = $art;
     }
@@ -63,6 +66,8 @@ class Animation extends DynamicTerminalObject
      */
     public function scroll($direction = 'right')
     {
+        $this->setupKeyframes();
+
         $mapping = [
             'left'   => 'right',
             'right'  => 'left',
@@ -76,8 +81,12 @@ class Animation extends DynamicTerminalObject
             return false;
         }
 
-        $keyframes   = $this->enterKeyframes($mapping[$direction]);
-        $keyframes   = array_merge($keyframes, $this->exitKeyframes($mapping[$mapping[$direction]]));
+        $lines       = $this->getLines();
+        $enter_from  = $mapping[$direction];
+        $exit_to     = $mapping[$enter_from];
+
+        $keyframes   = $this->keyframes->enterFrom($lines, $enter_from);
+        $keyframes   = array_merge($keyframes, $this->keyframes->exitTo($lines, $exit_to));
         $keyframes   = array_unique($keyframes, SORT_REGULAR);
         $keyframes[] = reset($keyframes);
 
@@ -91,7 +100,9 @@ class Animation extends DynamicTerminalObject
      */
     public function exitTo($direction)
     {
-        $this->animate($this->exitKeyframes($direction));
+        $this->setupKeyframes();
+
+        $this->animate($this->keyframes->exitTo($this->getLines(), $direction));
     }
 
     /**
@@ -101,7 +112,14 @@ class Animation extends DynamicTerminalObject
      */
     public function enterFrom($direction)
     {
-        $this->animate($this->enterKeyframes($direction));
+        $this->setupKeyframes();
+
+        $this->animate($this->keyframes->enterFrom($this->getLines(), $direction));
+    }
+
+    protected function getLines()
+    {
+        return $this->parse($this->artFile($this->art));
     }
 
     /**
@@ -113,236 +131,20 @@ class Animation extends DynamicTerminalObject
     }
 
     /**
-     * Get the line parser for the direction
-     *
-     * @param string $direction
-     * @return string
+     * @param League\CLImate\TerminalObject\Dynamic\Animation\Keyframe $keyframes
      */
-    protected function getLineMethod($direction)
+    protected function setKeyFrames($keyframes)
     {
-        return 'current' . ucwords(strtolower($direction)) . 'Line';
+        $this->keyframes = $keyframes ?: new Keyframe;
     }
 
     /**
-     * Adjust the array of lines if necessary
-     *
-     * @param array $lines
-     * @param string $direction
-     *
-     * @return array
+     * Set up the necessary properties on the Keyframe class
      */
-    protected function adjustLines(array $lines, $direction)
+    protected function setupKeyframes()
     {
-        $adjust_method = 'adjust' . ucwords(strtolower($direction))  . 'Lines';
-
-        if (method_exists($this, $adjust_method)) {
-            return $this->$adjust_method($lines);
-        }
-
-        return $lines;
-    }
-
-    /**
-     * Pad the array of lines for "right" animation
-     *
-     * @param array $lines
-     * @return array
-     */
-    protected function adjustRightLines(array $lines)
-    {
-        return $this->padArray($lines, $this->util->width());
-    }
-
-    /**
-     * Pad the array of lines for "left" animation
-     *
-     * @param array $lines
-     * @return array
-     */
-    protected function adjustLeftLines(array $lines)
-    {
-        return $this->padArray($lines, $this->maxStrLen($lines));
-    }
-
-    /**
-     * Get the keyframes appropriate for the animation direction
-     *
-     * @param string $direction
-     * @param array $lines
-     * @param string $line_method
-     *
-     * @return array
-     */
-    protected function getDirectionKeyframes($direction, array $lines, $line_method)
-    {
-        $mapping = [
-            'exitHorizontalKeyframes' => ['left', 'right'],
-            'exitVerticalKeyFrames'   => ['top', 'bottom'],
-        ];
-
-        foreach ($mapping as $method => $directions) {
-            if (in_array($direction, $directions)) {
-                return $this->$method($lines, $line_method);
-            }
-        }
-
-        // Fail gracefully, simply return an array
-        return [];
-    }
-
-    /**
-     * Get the enter keyframes for the desired direction
-     *
-     * @param string $direction
-     *
-     * @return array
-     */
-    protected function enterKeyframes($direction)
-    {
-        return array_reverse($this->exitKeyframes($direction));
-    }
-
-    /**
-     * Get the exit keyframes for the desired direction
-     *
-     * @param string $direction
-     *
-     * @return array
-     */
-    protected function exitKeyframes($direction)
-    {
-        $lines       = $this->parse($this->artFile($this->art));
-        $lines       = $this->adjustLines($lines, $direction);
-        $line_method = $this->getLineMethod($direction);
-
-        $direction_keyframes = $this->getDirectionKeyframes($direction, $lines, $line_method);
-
-        $keyframes   = array_fill(0, 4, $lines);
-        $keyframes   = array_merge($keyframes, $direction_keyframes);
-        $keyframes[] = array_fill(0, count($lines), '');
-
-        return $keyframes;
-    }
-
-    /**
-     * Create horizontal exit animation keyframes for the art
-     *
-     * @param array $lines
-     * @param string $line_method
-     *
-     * @return array
-     */
-    protected function exitHorizontalKeyframes(array $lines, $line_method)
-    {
-        $keyframes = [];
-        $length    = strlen($lines[0]);
-
-        for ($i = $length; $i > 0; $i--) {
-            $keyframes[] = $this->getHorizontalKeyframe($lines, $i, $line_method, $length);
-        }
-
-        return $keyframes;
-    }
-
-    /**
-     * Get the keyframe for a horizontal animation
-     *
-     * @param array $lines
-     * @param int $frame_number
-     * @param string $line_method
-     * @param int $length
-     *
-     * @return array
-     */
-    protected function getHorizontalKeyframe(array $lines, $frame_number, $line_method, $length)
-    {
-        $keyframe = [];
-
-        foreach ($lines as $line) {
-            $keyframe[] = $this->$line_method($line, $frame_number, $length);
-        }
-
-        return $keyframe;
-    }
-
-    /**
-     * Create vertical exit animation keyframes for the art
-     *
-     * @param array $lines
-     * @param string $line_method
-     *
-     * @return array
-     */
-    protected function exitVerticalKeyFrames(array $lines, $line_method)
-    {
-        $keyframes  = [];
-        $line_count = count($lines);
-
-        for ($i = $line_count - 1; $i >= 0; $i--) {
-            $keyframes[] = $this->$line_method($lines, $line_count, $i);
-        }
-
-        return $keyframes;
-    }
-
-    /**
-     * Get the current line as it is exiting left
-     *
-     * @param string $line
-     * @param int $frame_number
-     *
-     * @return string
-     */
-    protected function currentLeftLine($line, $frame_number)
-    {
-        return substr($line, -$frame_number);
-    }
-
-
-    /**
-     * Get the current line as it is exiting right
-     *
-     * @param string $line
-     * @param int $frame_number
-     * @param int $length
-     *
-     * @return string
-     */
-    protected function currentRightLine($line, $frame_number, $length)
-    {
-        return str_repeat(' ', $length - $frame_number) . substr($line, 0, $frame_number);
-    }
-
-    /**
-     * Slice off X number of lines from the bottom and fill the rest with empty strings
-     *
-     * @param array $lines
-     * @param integer $total_lines
-     * @param integer $current
-     *
-     * @return array
-     */
-    protected function currentTopLine($lines, $total_lines, $current)
-    {
-        $keyframe = array_slice($lines, -$current, $current);
-
-        return array_merge($keyframe, array_fill(0, $total_lines - $current, ''));
-    }
-
-    /**
-     * Slice off X number of lines from the top and fill the rest with empty strings
-     *
-     * @param array $lines
-     * @param integer $total_lines
-     * @param integer $current
-     *
-     * @return array
-     */
-    protected function currentBottomLine($lines, $total_lines, $current)
-    {
-        $keyframe = array_fill(0, $total_lines - $current, '');
-
-        return array_merge($keyframe, array_slice($lines, 0, $current));
+        $this->keyframes->parser($this->parser);
+        $this->keyframes->util($this->util);
     }
 
     /**
---
+++ src/TerminalObject/Dynamic/Animation/Keyframe.php
@@ -0,0 +1,245 @@
+<?php
+
+namespace League\CLImate\TerminalObject\Dynamic\Animation;
+
+use League\CLImate\Decorator\Parser\ParserImporter;
+use League\CLImate\TerminalObject\Helper\StringLength;
+use League\CLImate\Util\UtilImporter;
+
+class Keyframe
+{
+    use StringLength, ParserImporter, UtilImporter;
+
+    /**
+     * Get the enter keyframes for the desired direction
+     *
+     * @param string $direction
+     *
+     * @return array
+     */
+    public function enterFrom($lines, $direction)
+    {
+        return array_reverse($this->exitTo($lines, $direction));
+    }
+
+    /**
+     * Get the exit keyframes for the desired direction
+     *
+     * @param string $direction
+     *
+     * @return array
+     */
+    public function exitTo($lines, $direction)
+    {
+        $lines       = $this->adjustLines($lines, $direction);
+        $line_method = $this->getLineMethod($direction);
+
+        $direction_keyframes = $this->getDirectionFrames($direction, $lines, $line_method);
+
+        $keyframes   = array_fill(0, 4, $lines);
+        $keyframes   = array_merge($keyframes, $direction_keyframes);
+        $keyframes[] = array_fill(0, count($lines), '');
+
+        return $keyframes;
+    }
+
+    /**
+     * Get the line parser for the direction
+     *
+     * @param string $direction
+     * @return string
+     */
+    protected function getLineMethod($direction)
+    {
+        return 'current' . ucwords(strtolower($direction)) . 'Line';
+    }
+
+    /**
+     * Adjust the array of lines if necessary
+     *
+     * @param array $lines
+     * @param string $direction
+     *
+     * @return array
+     */
+    protected function adjustLines(array $lines, $direction)
+    {
+        $adjust_method = 'adjust' . ucwords(strtolower($direction))  . 'Lines';
+
+        if (method_exists($this, $adjust_method)) {
+            return $this->$adjust_method($lines);
+        }
+
+        return $lines;
+    }
+
+    /**
+     * Pad the array of lines for "right" animation
+     *
+     * @param array $lines
+     * @return array
+     */
+    protected function adjustRightLines(array $lines)
+    {
+        return $this->padArray($lines, $this->util->width());
+    }
+
+    /**
+     * Pad the array of lines for "left" animation
+     *
+     * @param array $lines
+     * @return array
+     */
+    protected function adjustLeftLines(array $lines)
+    {
+        return $this->padArray($lines, $this->maxStrLen($lines));
+    }
+
+    /**
+     * Get the keyframes appropriate for the animation direction
+     *
+     * @param string $direction
+     * @param array $lines
+     * @param string $line_method
+     *
+     * @return array
+     */
+    protected function getDirectionFrames($direction, array $lines, $line_method)
+    {
+        $mapping = [
+            'exitHorizontalFrames' => ['left', 'right'],
+            'exitVerticalFrames'   => ['top', 'bottom'],
+        ];
+
+        foreach ($mapping as $method => $directions) {
+            if (in_array($direction, $directions)) {
+                return $this->$method($lines, $line_method);
+            }
+        }
+
+        // Fail gracefully, simply return an array
+        return [];
+    }
+
+    /**
+     * Create horizontal exit animation keyframes for the art
+     *
+     * @param array $lines
+     * @param string $line_method
+     *
+     * @return array
+     */
+    protected function exitHorizontalFrames(array $lines, $line_method)
+    {
+        $keyframes = [];
+        $length    = strlen($lines[0]);
+
+        for ($i = $length; $i > 0; $i--) {
+            $keyframes[] = $this->getHorizontalKeyframe($lines, $i, $line_method, $length);
+        }
+
+        return $keyframes;
+    }
+
+    /**
+     * Get the keyframe for a horizontal animation
+     *
+     * @param array $lines
+     * @param int $frame_number
+     * @param string $line_method
+     * @param int $length
+     *
+     * @return array
+     */
+    protected function getHorizontalKeyframe(array $lines, $frame_number, $line_method, $length)
+    {
+        $keyframe = [];
+
+        foreach ($lines as $line) {
+            $keyframe[] = $this->$line_method($line, $frame_number, $length);
+        }
+
+        return $keyframe;
+    }
+
+    /**
+     * Create vertical exit animation keyframes for the art
+     *
+     * @param array $lines
+     * @param string $line_method
+     *
+     * @return array
+     */
+    protected function exitVerticalFrames(array $lines, $line_method)
+    {
+        $keyframes  = [];
+        $line_count = count($lines);
+
+        for ($i = $line_count - 1; $i >= 0; $i--) {
+            $keyframes[] = $this->$line_method($lines, $line_count, $i);
+        }
+
+        return $keyframes;
+    }
+
+    /**
+     * Get the current line as it is exiting left
+     *
+     * @param string $line
+     * @param int $frame_number
+     *
+     * @return string
+     */
+    protected function currentLeftLine($line, $frame_number)
+    {
+        return substr($line, -$frame_number);
+    }
+
+
+    /**
+     * Get the current line as it is exiting right
+     *
+     * @param string $line
+     * @param int $frame_number
+     * @param int $length
+     *
+     * @return string
+     */
+    protected function currentRightLine($line, $frame_number, $length)
+    {
+        return str_repeat(' ', $length - $frame_number) . substr($line, 0, $frame_number);
+    }
+
+    /**
+     * Slice off X number of lines from the bottom and fill the rest with empty strings
+     *
+     * @param array $lines
+     * @param integer $total_lines
+     * @param integer $current
+     *
+     * @return array
+     */
+    protected function currentTopLine($lines, $total_lines, $current)
+    {
+        $keyframe = array_slice($lines, -$current, $current);
+
+        return array_merge($keyframe, array_fill(0, $total_lines - $current, ''));
+    }
+
+    /**
+     * Slice off X number of lines from the top and fill the rest with empty strings
+     *
+     * @param array $lines
+     * @param integer $total_lines
+     * @param integer $current
+     *
+     * @return array
+     */
+    protected function currentBottomLine($lines, $total_lines, $current)
+    {
+        $keyframe = array_fill(0, $total_lines - $current, '');
+
+        return array_merge($keyframe, array_slice($lines, 0, $current));
+    }
+
+}
