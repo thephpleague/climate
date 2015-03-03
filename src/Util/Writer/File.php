0d21c16002c5fa585c63eff147b4f199166d67d6
Terah <terry@terah.com.au>
2015-03-03 15:21:03 +1000

    Adding file writer

---
+++ src/Util/Writer/File.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace League\CLImate\Util\Writer;
+
+
+class File implements WriterInterface
+{
+    /* @var resource */
+    protected $resource;
+
+    /**
+     * @param $resource
+     */
+    public function __construct($resource)
+    {
+        $this->resource = $resource;
+    }
+    /**
+     * Write the content to the stream
+     *
+     * @param  string $content
+     */
+    public function write($content)
+    {
+        fwrite($this->resource, $content);
+    }
+
+    public function _destruct()
+    {
+        fclose($this->resource);
+    }
+}
