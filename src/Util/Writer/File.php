7bf605d96249b70a6f26f6a9cad8a09787eb4c32
Terah <terry@terah.com.au>
2015-03-03 19:45:47 +1000

    Enhancing file writer

--- src/Util/Writer/File.php
+++ src/Util/Writer/File.php
@@ -5,15 +5,44 @@ namespace League\CLImate\Util\Writer;
 
 class File implements WriterInterface
 {
-    /* @var resource */
+    /* @var resource|string */
     protected $resource;
 
+    /* @var boolean $close_locally */
+    protected $close_locally = false;
+
+    /* @var boolean $use_locking */
+    protected $use_locking = false;
+
+    /* @var boolean $gzip_file */
+    protected $gzip_file = false;
+
     /**
-     * @param $resource
+     * @param string|resource $resource
+     * @param bool $use_locking
+     * @param bool $gzip_file
      */
-    public function __construct($resource)
+    public function __construct($resource, $use_locking=false, $gzip_file=false)
     {
-        $this->resource = $resource;
+        $this->resource     = $resource;
+        $this->use_locking  = $use_locking;
+        $this->gzip_file    = $gzip_file;
+    }
+
+    protected function getResource()
+    {
+        if ( ! is_resource($this->resource) ) {
+            return $this->resource;
+        }
+        $this->close_locally = true;
+        if ( ! is_writable($this->resource) ) {
+            throw new \Exception("The resource {$this->resource} is not writable");
+        }
+        $this->resource = $this->gzip_file ? gzopen($this->resource, 'a') : fopen($this->resource, 'a');
+        if ( ! $this->resource ) {
+            throw new \Exception("The resource could not be opened");
+        }
+        return $this->resource;
     }
 
     /**
@@ -23,11 +52,13 @@ class File implements WriterInterface
      */
     public function write($content)
     {
-        fwrite($this->resource, $content);
+        gzwrite($this->getResource(), $content);
     }
 
     public function _destruct()
     {
-        fclose($this->resource);
+        if ( $this->close_locally ) {
+            gzclose($this->getResource());
+        }
     }
 }
