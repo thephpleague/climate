5a38b2c04537983cebd4b7c1237ad9eb510a3264
Joe Tannenbaum <joe@joe.codes>
2015-03-03 06:51:08 -0500

    small refactor, added some helper setter functions

--- src/Util/Writer/File.php
+++ src/Util/Writer/File.php
@@ -5,16 +5,16 @@ namespace League\CLImate\Util\Writer;
 
 class File implements WriterInterface
 {
-    /* @var resource|string */
+    /** @var resource|string */
     protected $resource;
 
-    /* @var boolean $close_locally */
+    /** @var boolean $close_locally */
     protected $close_locally = false;
 
-    /* @var boolean $use_locking */
+    /** @var boolean $use_locking */
     protected $use_locking = false;
 
-    /* @var boolean $gzip_file */
+    /** @var boolean $gzip_file */
     protected $gzip_file = false;
 
     /**
@@ -22,27 +22,25 @@ class File implements WriterInterface
      * @param bool $use_locking
      * @param bool $gzip_file
      */
-    public function __construct($resource, $use_locking=false, $gzip_file=false)
+    public function __construct($resource, $use_locking = false, $gzip_file = false)
     {
-        $this->resource     = $resource;
-        $this->use_locking  = $use_locking;
-        $this->gzip_file    = $gzip_file;
+        $this->resource    = $resource;
+        $this->use_locking = $use_locking;
+        $this->gzip_file   = $gzip_file;
     }
 
-    protected function getResource()
+    public function lock()
     {
-        if ( ! is_resource($this->resource) ) {
-            return $this->resource;
-        }
-        $this->close_locally = true;
-        if ( ! is_writable($this->resource) ) {
-            throw new \Exception("The resource {$this->resource} is not writable");
-        }
-        $this->resource = $this->gzip_file ? gzopen($this->resource, 'a') : fopen($this->resource, 'a');
-        if ( ! $this->resource ) {
-            throw new \Exception("The resource could not be opened");
-        }
-        return $this->resource;
+        $this->user_locking = true;
+
+        return $this;
+    }
+
+    public function gzipped()
+    {
+        $this->gzip_file = true;
+
+        return $this;
     }
 
     /**
@@ -53,18 +51,49 @@ class File implements WriterInterface
     public function write($content)
     {
         $resource = $this->getResource();
-        if ( $this->use_locking ) {
+
+        if ($this->use_locking) {
             flock($resource, LOCK_EX);
         }
+
         gzwrite($resource, $content);
-        if ( $this->use_locking ) {
+
+        if ($this->use_locking) {
             flock($resource, LOCK_UN);
         }
     }
 
+    protected function getResource()
+    {
+        if (is_resource($this->resource)) {
+            return $this->resource;
+        }
+
+        $this->close_locally = true;
+
+        if (!is_writable($this->resource)) {
+            throw new \Exception("The resource {$this->resource} is not writable");
+        }
+
+        if (!($this->resource = $this->openResource())) {
+            throw new \Exception("The resource could not be opened");
+        }
+
+        return $this->resource;
+    }
+
+    protected function openResource()
+    {
+        if ($this->gzip_file) {
+            return gzopen($this->resource, 'a');
+        }
+
+        return fopen($this->resource, 'a');
+    }
+
     public function _destruct()
     {
-        if ( $this->close_locally ) {
+        if ($this->close_locally) {
             gzclose($this->getResource());
         }
     }
