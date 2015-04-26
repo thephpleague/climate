a7a9119ef710a591ea18e6b1be1e70b9873cecb4
Joe Tannenbaum <joe@joe.codes>
2015-04-26 17:49:09 -0400

    linux tests for added methods related to password prompt

--- tests/LinuxTest.php
+++ tests/LinuxTest.php
@@ -6,7 +6,6 @@ class LinuxTest extends TestBase
 {
 
     /** @test */
-
     public function it_can_get_the_width()
     {
         $system = Mockery::mock('League\CLImate\Util\System\Linux')
@@ -19,7 +18,6 @@ class LinuxTest extends TestBase
     }
 
     /** @test */
-
     public function it_will_return_null_if_width_is_not_numeric()
     {
         $system = Mockery::mock('League\CLImate\Util\System\Linux')
@@ -32,7 +30,6 @@ class LinuxTest extends TestBase
     }
 
     /** @test */
-
     public function it_can_get_the_height()
     {
         $system = Mockery::mock('League\CLImate\Util\System\Linux')
@@ -45,7 +42,6 @@ class LinuxTest extends TestBase
     }
 
     /** @test */
-
     public function it_will_return_null_if_height_is_not_numeric()
     {
         $system = Mockery::mock('League\CLImate\Util\System\Linux')
@@ -55,6 +51,44 @@ class LinuxTest extends TestBase
         $system->shouldReceive('exec')->with('tput lines')->andReturn('error');
 
         $this->assertNull($system->height());
+    }
+
+    /** @test */
+    public function it_can_check_for_bash_access()
+    {
+        $system = Mockery::mock('League\CLImate\Util\System\Linux')
+                            ->makePartial()
+                            ->shouldAllowMockingProtectedMethods();
+
+        $system->shouldReceive('exec')->with("/usr/bin/env bash -c 'echo OK'")->andReturn('OK');
+
+        $this->assertTrue($system->canAccessBash());
+    }
+
+    /** @test */
+    public function it_can_check_for_bash_access_even_when_not_available()
+    {
+        $system = Mockery::mock('League\CLImate\Util\System\Linux')
+                            ->makePartial()
+                            ->shouldAllowMockingProtectedMethods();
+
+        $system->shouldReceive('exec')->with("/usr/bin/env bash -c 'echo OK'")->andReturn('');
+
+        $this->assertFalse($system->canAccessBash());
+    }
+
+    /** @test */
+    public function it_can_output_a_prompt_with_a_hidden_response()
+    {
+        $system = Mockery::mock('League\CLImate\Util\System\Linux')
+                            ->makePartial()
+                            ->shouldAllowMockingProtectedMethods();
+
+        $system->shouldReceive('exec')
+                    ->with("/usr/bin/env bash -c 'read -s -p \"Password Please:\" response && echo \$response'")
+                    ->andReturn('the response   ');
+
+        $this->assertSame($system->hiddenResponsePrompt('Password Please:'), 'the response');
     }
 
 }
