b7ce765ccd0ae69a68616717d4aa362033620d73
Joe Tannenbaum <josephtannenbaum@gmail.com>
2015-07-28 22:53:47 -0400

    make sure it can write to a gzipped file

--- tests/FileTest.php
+++ tests/FileTest.php
@@ -72,6 +72,23 @@ class FileTest extends TestBase
     }
 
     /** @test */
+    public function it_can_write_to_a_gzipped_file()
+    {
+        $resource = fopen($this->file->url(), 'a');
+        $file     = $this->getFileMock($resource);
+
+        $file->gzipped();
+
+        $output = new League\CLImate\Util\Output();
+        $output->add('file', $file);
+        $output->defaultTo('file');
+
+        $output->write("Oh, you're still here.");
+
+        $this->assertSame("Oh, you're still here.\n", $this->file->getContent());
+    }
+
+    /** @test */
     public function it_will_yell_when_a_non_writable_resource_is_passed()
     {
         $this->file->chmod(0444);
