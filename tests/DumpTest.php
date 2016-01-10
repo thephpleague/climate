e19e992fc240addd8948f32661975b61b466f387
Joe Tannenbaum <joe@joe.codes>
2016-01-09 19:08:36 -0500

    fixed dump test for php7 by overriding var_dump in namespace

--- tests/DumpTest.php
+++ tests/DumpTest.php
@@ -2,17 +2,14 @@
 
 namespace League\CLImate\Tests;
 
+require_once 'VarDumpMock.php';
+
 class DumpTest extends TestBase
 {
     /** @test */
     public function it_can_dump_a_variable()
     {
-        $should_be = [
-            'string(10) "This thing"',
-            ''
-        ];
-
-        $this->shouldWrite("\e[m" . implode("\n", $should_be) . "\e[0m");
+        $this->shouldWrite("\e[mDUMPED: This thing\e[0m");
         $this->shouldHavePersisted();
 
         $this->cli->dump('This thing');
---
+++ tests/VarDumpMock.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace League\CLImate\TerminalObject\Basic;
+
+function var_dump($arg)
+{
+    echo 'DUMPED: ' . $arg;
+}
