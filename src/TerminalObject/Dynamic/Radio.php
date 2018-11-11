824466c3c00655261fe5aca40f137a444dced2e0
Maxime Renou <contact@maximerenou.fr>
2018-11-11 14:41:21 +0100

    Radio system improvement

--- src/TerminalObject/Dynamic/Radio.php
+++ src/TerminalObject/Dynamic/Radio.php
@@ -15,4 +15,40 @@ class Radio extends Checkboxes
     {
         return new Checkbox\RadioGroup($options);
     }
+
+    /**
+     * Take the appropriate action based on the input character,
+     * returns whether to stop listening or not
+     *
+     * @param string $char
+     *
+     * @return bool
+     */
+    protected function handleCharacter($char)
+    {
+        switch ($char) {
+            case ' ':
+            case "\n":
+                $this->checkboxes->toggleCurrent();
+                $this->output->sameLine()->write($this->util->cursor->defaultStyle());
+                $this->output->sameLine()->write("\e[0m");
+                return true; // Break the while loop as well
+
+            case "\e":
+                $this->handleAnsi();
+                break;
+        }
+
+        return false;
+    }
+
+    /**
+     * Format the prompt string
+     *
+     * @return string
+     */
+    protected function promptFormatted()
+    {
+        return $this->prompt . ' (press <Enter> to select)';
+    }
 }
