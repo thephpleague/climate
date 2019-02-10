4d33f4e0020f35d3ebd84de4630a0d6736d4801b
Craig Duncan <git@duncanc.co.uk>
2019-02-10 17:44:07 +0000

    Allow the confirm default to be overridden

--- src/TerminalObject/Dynamic/Confirm.php
+++ src/TerminalObject/Dynamic/Confirm.php
@@ -2,11 +2,26 @@
 
 namespace League\CLImate\TerminalObject\Dynamic;
 
+use League\CLImate\Util\Reader\ReaderInterface;
+use function in_array;
 use function strtolower;
 use function substr;
 
 class Confirm extends Input
 {
+
+
+    /**
+     * @inheritdoc
+     */
+    public function __construct($prompt, ReaderInterface $reader = null)
+    {
+        parent::__construct($prompt, $reader);
+
+        $this->default = "n";
+    }
+
+
     /**
      * Let us know if the user confirmed.
      *
@@ -14,8 +29,11 @@ class Confirm extends Input
      */
     public function confirmed()
     {
-        $this->prompt .= " [y/N]";
-        $this->default = "n";
+        if (in_array($this->default, ["y", "yes"], true)) {
+            $this->prompt .= " [Y/n]";
+        } else {
+            $this->prompt .= " [y/N]";
+        }
 
         $this->accept(['y', 'yes', 'n', 'no'], false);
 
