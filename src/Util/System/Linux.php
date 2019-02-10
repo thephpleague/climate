dea263605a84267c4473df825a64f62b9e3ec9b7
Saif Eddin G <29315886+azjezz@users.noreply.github.com>
2019-02-10 08:54:46 -0800

    Better check for Ansi color support on Linux. (#129)

--- src/Util/System/Linux.php
+++ src/Util/System/Linux.php
@@ -83,6 +83,22 @@ class Linux extends System
      */
     protected function systemHasAnsiSupport()
     {
-        return (function_exists('posix_isatty') && @posix_isatty(STDOUT));
+        if ('Hyper' === getenv('TERM_PROGRAM')) {
+            return true;
+        }
+        
+        $stream = STDOUT;
+        
+        if (function_exists('stream_isatty')) {
+            return @stream_isatty($stream);
+        }
+
+        if (function_exists('posix_isatty')) {
+            return @posix_isatty($stream);
+        }
+
+        $stat = @fstat($stream);
+        // Check if formatted mode is S_IFCHR
+        return $stat ? 0020000 === ($stat['mode'] & 0170000) : false;
     }
 }
